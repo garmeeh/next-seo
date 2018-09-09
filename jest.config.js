52fd37df84a01e0c3c656fc6e3f5db62e2c3b399
Gary Meehan <garymeehan@outlook.com>
2018-09-09 19:21:50 +0100

    ignore dist folder to jest to stop tests breaking

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.history/', '<rootDir>/node_modules/', '<rootDir>/e2e/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.history/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/e2e/',
+    '<rootDir>/dist/',
+  ],
 };
