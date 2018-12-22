2a9ec46345cce162039b81fa1722de103a2ae35d
econdie <erik@7cups.com>
2018-12-22 01:01:28 -0500

    strict equality on og type. added tests and examples

--- src/meta/buildTags.jsx
+++ src/meta/buildTags.jsx
@@ -89,81 +89,166 @@ const buildTags = config => {
     }
 
     if (config.openGraph.type) {
-      tagsToRender.push(<meta key="og:type" property="og:type" content={config.openGraph.type} />);
+      const type = config.openGraph.type.toLowerCase();
 
-      if (config.openGraph.type == 'profile' && config.openGraph.profile) {
-        if (config.openGraph.profile.first_name) {
-          tagsToRender.push(<meta key="profile:first_name" property="profile:first_name" content={config.openGraph.profile.first_name} />);
+      tagsToRender.push(
+        <meta key="og:type" property="og:type" content={type} />,
+      );
+
+      if (type === 'profile' && config.openGraph.profile) {
+        if (config.openGraph.profile.firstName) {
+          tagsToRender.push(
+            <meta
+              key="profile:first_name"
+              property="profile:first_name"
+              content={config.openGraph.profile.firstName}
+            />,
+          );
         }
 
-        if (config.openGraph.profile.last_name) {
-          tagsToRender.push(<meta key="profile:last_name" property="profile:last_name" content={config.openGraph.profile.last_name} />);
+        if (config.openGraph.profile.lastName) {
+          tagsToRender.push(
+            <meta
+              key="profile:last_name"
+              property="profile:last_name"
+              content={config.openGraph.profile.lastName}
+            />,
+          );
         }
 
         if (config.openGraph.profile.username) {
-          tagsToRender.push(<meta key="profile:username" property="profile:username" content={config.openGraph.profile.username} />);
+          tagsToRender.push(
+            <meta
+              key="profile:username"
+              property="profile:username"
+              content={config.openGraph.profile.username}
+            />,
+          );
         }
 
         if (config.openGraph.profile.gender) {
-          tagsToRender.push(<meta key="profile:gender" property="profile:gender" content={config.openGraph.profile.gender} />);
+          tagsToRender.push(
+            <meta
+              key="profile:gender"
+              property="profile:gender"
+              content={config.openGraph.profile.gender}
+            />,
+          );
         }
-      }
-
-      if (config.openGraph.type == 'book' && config.openGraph.book) {
-        if (config.openGraph.book.author && config.openGraph.book.author.length) {
-          config.openGraph.book.author.forEach((author, index) => {
+      } else if (type === 'book' && config.openGraph.book) {
+        if (
+          config.openGraph.book.authors &&
+          config.openGraph.book.authors.length
+        ) {
+          config.openGraph.book.authors.forEach((author, index) => {
             tagsToRender.push(
-              <meta key={`book:author:0${index}`} property="book:author" content={author} />,
+              <meta
+                key={`book:author:0${index}`}
+                property="book:author"
+                content={author}
+              />,
             );
           });
         }
 
         if (config.openGraph.book.isbn) {
-          tagsToRender.push(<meta key="book:isbn" property="book:isbn" content={config.openGraph.book.isbn} />);
+          tagsToRender.push(
+            <meta
+              key="book:isbn"
+              property="book:isbn"
+              content={config.openGraph.book.isbn}
+            />,
+          );
         }
 
-        if (config.openGraph.book.release_date) {
-          tagsToRender.push(<meta key="book:release_date" property="book:release_date" content={config.openGraph.book.release_date} />);
+        if (config.openGraph.book.releaseDate) {
+          tagsToRender.push(
+            <meta
+              key="book:release_date"
+              property="book:release_date"
+              content={config.openGraph.book.releaseDate}
+            />,
+          );
         }
 
-        if (config.openGraph.book.tag && config.openGraph.book.tag.length) {
-          config.openGraph.book.tag.forEach((tag, index) => {
+        if (config.openGraph.book.tags && config.openGraph.book.tags.length) {
+          config.openGraph.book.tags.forEach((tag, index) => {
             tagsToRender.push(
-              <meta key={`book:tag:0${index}`} property="book:tag" content={tag} />,
+              <meta
+                key={`book:tag:0${index}`}
+                property="book:tag"
+                content={tag}
+              />,
             );
           });
         }
-      }
-
-      if (config.openGraph.type == 'article' && config.openGraph.article) {
-        if (config.openGraph.article.published_time) {
-          tagsToRender.push(<meta key="article:published_time" property="article:published_time" content={config.openGraph.article.published_time} />);
+      } else if (type === 'article' && config.openGraph.article) {
+        if (config.openGraph.article.publishedTime) {
+          tagsToRender.push(
+            <meta
+              key="article:published_time"
+              property="article:published_time"
+              content={config.openGraph.article.publishedTime}
+            />,
+          );
         }
 
-        if (config.openGraph.article.modified_time) {
-          tagsToRender.push(<meta key="article:modified_time" property="article:modified_time" content={config.openGraph.article.modified_time} />);
+        if (config.openGraph.article.modifiedTime) {
+          tagsToRender.push(
+            <meta
+              key="article:modified_time"
+              property="article:modified_time"
+              content={config.openGraph.article.modifiedTime}
+            />,
+          );
         }
 
-        if (config.openGraph.article.expiration_time) {
-          tagsToRender.push(<meta key="article:expiration_time" property="article:expiration_time" content={config.openGraph.article.expiration_time} />);
+        if (config.openGraph.article.expirationTime) {
+          tagsToRender.push(
+            <meta
+              key="article:expiration_time"
+              property="article:expiration_time"
+              content={config.openGraph.article.expirationTime}
+            />,
+          );
         }
 
-        if (config.openGraph.article.author && config.openGraph.article.author.length) {
-          config.openGraph.article.author.forEach((author, index) => {
+        if (
+          config.openGraph.article.authors &&
+          config.openGraph.article.authors.length
+        ) {
+          config.openGraph.article.authors.forEach((author, index) => {
             tagsToRender.push(
-              <meta key={`article:author:0${index}`} property="article:author" content={author} />,
+              <meta
+                key={`article:author:0${index}`}
+                property="article:author"
+                content={author}
+              />,
             );
           });
         }
 
         if (config.openGraph.article.section) {
-          tagsToRender.push(<meta key="article:section" property="article:section" content={config.openGraph.article.section} />);
+          tagsToRender.push(
+            <meta
+              key="article:section"
+              property="article:section"
+              content={config.openGraph.article.section}
+            />,
+          );
         }
 
-        if (config.openGraph.article.tag && config.openGraph.article.tag.length) {
-          config.openGraph.article.tag.forEach((tag, index) => {
+        if (
+          config.openGraph.article.tags &&
+          config.openGraph.article.tags.length
+        ) {
+          config.openGraph.article.tags.forEach((tag, index) => {
             tagsToRender.push(
-              <meta key={`article:tag:0${index}`} property="article:tag" content={tag} />,
+              <meta
+                key={`article:tag:0${index}`}
+                property="article:tag"
+                content={tag}
+              />,
             );
           });
         }
