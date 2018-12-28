8647f6c56e28238cdb47c59ef70813186d42922b
Jerome Fitzgerald <jeromes@gmail.com>
2018-12-27 20:16:35 -0500

    🐠️ babel@7 ✨️ taskr 📦️ upgrades
    🐠️ babel@7
    - ⬆️ Upgrade from babel@6
    
    ✨️ taskr
    - Instead of babel/cli
    
    📦️ package.json
    - ⬆️ upgrades
    
    TODO: Verify Major Version Upgrades of:
    - eslint-plugin-jest@22.1.2
    - react-testing-library@5.4.2
    - regenerator-runtime@0.13.1

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-react-jsx',
+  ],
+};
