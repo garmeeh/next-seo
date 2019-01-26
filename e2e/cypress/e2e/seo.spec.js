59211ded2187a7778218c7446e98de6c5b5ff7e5
Gary Meehan <garymeehan@outlook.com>
2019-01-26 09:39:10 +0000

    update tests

--- e2e/cypress/e2e/seo.spec.js
+++ e2e/cypress/e2e/seo.spec.js
@@ -21,12 +21,12 @@ describe('SEO Meta', () => {
     cy.get('head meta[name="robots"]').should(
       'have.attr',
       'content',
-      'noindex,nofollow',
+      'index,follow',
     );
     cy.get('head meta[name="googlebot"]').should(
       'have.attr',
       'content',
-      'noindex,nofollow',
+      'index,follow',
     );
     cy.get('head meta[property="og:type"]').should(
       'have.attr',
@@ -135,12 +135,12 @@ describe('SEO Meta', () => {
     cy.get('head meta[name="robots"]').should(
       'have.attr',
       'content',
-      'index,follow',
+      'noindex,nofollow',
     );
     cy.get('head meta[name="googlebot"]').should(
       'have.attr',
       'content',
-      'index,follow',
+      'noindex,nofollow',
     );
     cy.get('head meta[property="og:type"]').should(
       'have.attr',
@@ -253,12 +253,12 @@ describe('SEO Meta', () => {
     cy.get('head meta[name="robots"]').should(
       'have.attr',
       'content',
-      'noindex,nofollow',
+      'index,follow',
     );
     cy.get('head meta[name="googlebot"]').should(
       'have.attr',
       'content',
-      'noindex,nofollow',
+      'index,follow',
     );
     cy.get('head meta[property="og:type"]').should(
       'have.attr',
