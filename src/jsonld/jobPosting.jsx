97a7f7708e900ef8b428db2bdfab2dba097fcd9b
Gary Meehan <garymeehan@outlook.com>
2019-06-20 20:21:42 +0100

    tweak formatting

--- src/jsonld/jobPosting.jsx
+++ src/jsonld/jobPosting.jsx
@@ -4,24 +4,17 @@ import Head from 'next/head';
 
 import markup from '../utils/markup';
 
-const buildBaseSalary = baseSalary =>
-  `
-        "baseSalary": {
-            "@type": "MonetaryAmount",
-            ${
-              baseSalary.currency ? `"currency": "${baseSalary.currency}",` : ''
-            }
-            "value": {
-                "@type": "QuantitativeValue",
-                ${baseSalary.value ? `"value": "${baseSalary.value}",` : ''}
-                ${
-                  baseSalary.unitText
-                    ? `"unitText": "${baseSalary.unitText}"`
-                    : ''
-                }
-            }
-        },
-    `;
+const buildBaseSalary = baseSalary => `
+  "baseSalary": {
+    "@type": "MonetaryAmount",
+    ${baseSalary.currency ? `"currency": "${baseSalary.currency}",` : ''}
+    "value": {
+      "@type": "QuantitativeValue",
+      ${baseSalary.value ? `"value": "${baseSalary.value}",` : ''}
+      ${baseSalary.unitText ? `"unitText": "${baseSalary.unitText}"` : ''}
+    }
+  },
+`;
 
 const JobPostingJsonLd = ({
   baseSalary,
@@ -36,47 +29,47 @@ const JobPostingJsonLd = ({
   title,
   validThrough,
 }) => {
-  const jslonld = `{
-        "@context": "http://schema.org",
-        "@type": "JobPosting",
-        ${baseSalary ? buildBaseSalary(baseSalary) : ''}
-        "datePosted": "${datePosted}",
-        "description": "${description}",
-        ${
-          educationRequirements
-            ? `"educationRequirements": "${educationRequirements}",`
-            : ''
-        }
-        ${employmentType ? `"employmentType": "${employmentType}",` : ''}
-        "hiringOrganization" : {
-            "@type" : "Organization",
-            "name" : "${hiringOrganization.name}",
-            "sameAs" : "${hiringOrganization.sameAs}"
-        },
-        
-        "jobLocation": {
-            "@type": "Place",
-            "address": {
-                "@type": "PostalAddress",
-                "addressLocality": "${jobLocation.addressLocality}",
-                "addressRegion": "${jobLocation.addressRegion}",
-                "postalCode" : "${jobLocation.postalCode}",
-                "streetAddress" : "${jobLocation.streetAddress}",
-                "addressCountry" : "${jobLocation.addressCountry}"
-            }
-        },
-        ${
-          applicantLocationRequirements
-            ? ` "applicantLocationRequirements": {
-            "@type": "Country",
-            "name": "${applicantLocationRequirements}"
-        },`
-            : ''
-        }
-        ${jobLocationType ? `"jobLocationType": "${jobLocationType}",` : ''}
-        ${validThrough ? `"validThrough": "${validThrough}",` : ''}
-        "title": "${title}"
-    }`;
+const jslonld = `{
+    "@context": "http://schema.org",
+    "@type": "JobPosting",
+    ${baseSalary ? buildBaseSalary(baseSalary) : ''}
+    "datePosted": "${datePosted}",
+    "description": "${description}",
+    ${
+      educationRequirements
+        ? `"educationRequirements": "${educationRequirements}",`
+        : ''
+    }
+    ${employmentType ? `"employmentType": "${employmentType}",` : ''}
+    "hiringOrganization" : {
+      "@type" : "Organization",
+      "name" : "${hiringOrganization.name}",
+      "sameAs" : "${hiringOrganization.sameAs}"
+    },
+    
+    "jobLocation": {
+      "@type": "Place",
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "${jobLocation.addressLocality}",
+        "addressRegion": "${jobLocation.addressRegion}",
+        "postalCode" : "${jobLocation.postalCode}",
+        "streetAddress" : "${jobLocation.streetAddress}",
+        "addressCountry" : "${jobLocation.addressCountry}"
+      }
+    },
+    ${
+      applicantLocationRequirements
+        ? ` "applicantLocationRequirements": {
+        "@type": "Country",
+        "name": "${applicantLocationRequirements}"
+    },`
+        : ''
+    }
+    ${jobLocationType ? `"jobLocationType": "${jobLocationType}",` : ''}
+    ${validThrough ? `"validThrough": "${validThrough}",` : ''}
+    "title": "${title}"
+  }`;
 
   return (
     <Head>
