b093de05f688d6e3679726509a16360803438160
Myoxocephalus <cj.adolfsson@gmail.com>
2020-02-11 21:35:30 +0100

    Removed getInitialProps from e2e test app. (#164)

--- e2e/pages/_app.tsx
+++ e2e/pages/_app.tsx
@@ -5,15 +5,6 @@ import { DefaultSeo } from '../../lib';
 import SEO from '../next-seo.config';
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }: any) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return (
