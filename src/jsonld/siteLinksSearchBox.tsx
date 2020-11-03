bde322d5f5c2490163f09f14bc782e664ca419fa
Omar Dulaimi <o.m.dulaimi@gmail.com>
2020-11-03 23:15:45 +0200

    implement sitelinks searchbox schema

---
+++ src/jsonld/siteLinksSearchBox.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react';
+import Head from 'next/head';
+
+import markup from '../utils/markup';
+export interface PotentialAction {
+  target: string;
+  queryInput: string;
+}
+export interface SiteLinksSearchBoxJsonLdProps {
+  keyOverride?: string;
+  url: string;
+  potentialActions: PotentialAction[];
+}
+
+const SiteLinksSearchBoxJsonLd: FC<SiteLinksSearchBoxJsonLdProps> = ({
+  keyOverride,
+  url,
+  potentialActions = [],
+}) => {
+  const jslonld = `{
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    "url": ${url},
+    "potentialAction": [
+      ${potentialActions.map(
+        ({ target, queryInput }) => `{
+        "@type": "SearchAction",
+        "target": ${target}={${queryInput}},
+        "query-input": required name=${queryInput}
+      }`,
+      )}
+     ]
+  }`;
+
+  return (
+    <Head>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={markup(jslonld)}
+        key={`jsonld-siteLinksSearchBox${keyOverride ? `-${keyOverride}` : ''}`}
+      />
+    </Head>
+  );
+};
+
+export default SiteLinksSearchBoxJsonLd;
