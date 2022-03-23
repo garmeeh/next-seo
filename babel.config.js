073ee6ce9ac6d9480f5b61eca6c94e6c93fc95cc
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2022-03-23 10:26:38 +0200

    [932] Target older browsers in Babel config (#937)

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-env', { targets: '> 0.25%, not dead' }],
     '@babel/preset-react',
     '@babel/preset-typescript',
   ],
