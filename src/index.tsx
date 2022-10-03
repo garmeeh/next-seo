1adbbac9a77e10a7e45dd149e643671ce7ab23a6
Raynold van Heyningen <raynoldvanheyningen@gmail.com>
2022-10-03 02:12:54 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
-export { default as DefaultSeo } from './meta/defaultSEO';
-export { default as NextSeo } from './meta/nextSEO';
-
+export { ArticleJsonLdProps, default as ArticleJsonLd } from './jsonld/article';
+export { BrandJsonLdProps, default as BrandJsonLd } from './jsonld/brand';
 export {
-  default as CarouselJsonLd,
+  BreadCrumbJsonLdProps,
+  default as BreadcrumbJsonLd,
+} from './jsonld/breadcrumb';
+export {
   CarouselJsonLdProps,
+  default as CarouselJsonLd,
 } from './jsonld/carousel';
 export {
-  default as NewsArticleJsonLd,
-  NewsArticleJsonLdProps,
-} from './jsonld/newsarticle';
+  CollectionPageJsonLdProps,
+  default as CollectionPageJsonLd,
+} from './jsonld/collectionPage';
+export {
+  CorporateContactJsonLdProps,
+  default as CorporateContactJsonLd,
+} from './jsonld/corporateContact';
+export { CourseJsonLdProps, default as CourseJsonLd } from './jsonld/course';
+export { DatasetJsonLdProps, default as DatasetJsonLd } from './jsonld/dataset';
+export { default as EventJsonLd, EventJsonLdProps } from './jsonld/event';
+export { default as FAQPageJsonLd, FAQPageJsonLdProps } from './jsonld/faqPage';
+export { default as HowToJsonLd, HowToJsonLdProps } from './jsonld/howTo';
 export {
   default as JobPostingJsonLd,
   JobPostingJsonLdProps,
@@ -17,26 +29,30 @@ export {
   default as LocalBusinessJsonLd,
   LocalBusinessJsonLdProps,
 } from './jsonld/localBusiness';
-export { default as QAPageJsonLd, QAPageJsonLdProps } from './jsonld/qaPage';
+export { default as LogoJsonLd, LogoJsonLdProps } from './jsonld/logo';
+export {
+  default as NewsArticleJsonLd,
+  NewsArticleJsonLdProps,
+} from './jsonld/newsarticle';
+export {
+  default as OrganizationJsonLd,
+  OrganizationJsonLdProps,
+} from './jsonld/organization';
+export { default as ProductJsonLd, ProductJsonLdProps } from './jsonld/product';
 export {
   default as ProfilePageJsonLd,
   ProfilePageJsonLdProps,
 } from './jsonld/profilePage';
+export { default as QAPageJsonLd, QAPageJsonLdProps } from './jsonld/qaPage';
+export { default as RecipeJsonLd, RecipeJsonLdProps } from './jsonld/recipe';
 export {
   default as SiteLinksSearchBoxJsonLd,
   SiteLinksSearchBoxJsonLdProps,
 } from './jsonld/siteLinksSearchBox';
-export { default as RecipeJsonLd, RecipeJsonLdProps } from './jsonld/recipe';
-export { default as EventJsonLd, EventJsonLdProps } from './jsonld/event';
 export {
-  default as CorporateContactJsonLd,
-  CorporateContactJsonLdProps,
-} from './jsonld/corporateContact';
-export {
-  default as CollectionPageJsonLd,
-  CollectionPageJsonLdProps,
-} from './jsonld/collectionPage';
-export { default as ProductJsonLd, ProductJsonLdProps } from './jsonld/product';
+  default as SocialProfileJsonLd,
+  SocialProfileJsonLdProps,
+} from './jsonld/socialProfile';
 export {
   default as SoftwareAppJsonLd,
   SoftwareAppJsonLdProps,
@@ -46,25 +62,9 @@ export {
   default as VideoGameJsonLd,
   VideoGameJsonLdProps,
 } from './jsonld/videoGame';
-export {
-  default as OrganizationJsonLd,
-  OrganizationJsonLdProps,
-} from './jsonld/organization';
-export { default as FAQPageJsonLd, FAQPageJsonLdProps } from './jsonld/faqPage';
-export { default as LogoJsonLd, LogoJsonLdProps } from './jsonld/logo';
-export { default as DatasetJsonLd, DatasetJsonLdProps } from './jsonld/dataset';
-export { default as CourseJsonLd, CourseJsonLdProps } from './jsonld/course';
-export {
-  default as BreadcrumbJsonLd,
-  BreadCrumbJsonLdProps,
-} from './jsonld/breadcrumb';
-export { default as BrandJsonLd, BrandJsonLdProps } from './jsonld/brand';
-export { default as ArticleJsonLd, ArticleJsonLdProps } from './jsonld/article';
 export { default as WebPageJsonLd, WebPageJsonLdProps } from './jsonld/webPage';
-export {
-  default as SocialProfileJsonLd,
-  SocialProfileJsonLdProps,
-} from './jsonld/socialProfile';
-export { default as HowToJsonLd, HowToJsonLdProps } from './jsonld/howTo';
-
+export { default as DefaultSeo } from './meta/defaultSEO';
+export { default as NextSeo } from './meta/nextSEO';
 export { DefaultSeoProps, NextSeoProps } from './types';
+
+console.log('test');
