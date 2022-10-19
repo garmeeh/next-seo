3c7a8b2d3af48b9745d3729294edad5448b65b81
Richard <richardcornelissen@users.noreply.github.com>
2022-10-19 09:20:41 +0200

    refactor: rewrite setAddress
    Co-authored-by: Gary Meehan <garymeehan@outlook.com>

--- src/utils/schema/setAddress.ts
+++ src/utils/schema/setAddress.ts
@@ -2,15 +2,14 @@ import { Address } from 'src/types';
 
 export function setAddress(address?: Address | Address[]) {
   if (!address) return undefined;
+  
+  if (!Array.isArray(address)) return toPostalAddress(address);
 
   // If array of one address, replace with single address
-  if (Array.isArray(address) && address.length === 1) address = address[0];
+  if (address.length === 1) return toPostalAddress(address[0]);
 
   // If array, return mapped array of PostalAddresses
-  if (Array.isArray(address)) return address.map(toPostalAddress);
-
-  // Return single PostalAddress
-  return toPostalAddress(address);
+  return address.map(toPostalAddress);
 }
 
 function toPostalAddress(address: Address) {
