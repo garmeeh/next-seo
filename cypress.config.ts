e9c252850a6d31c2839f64545d61d15c94041db6
Gary Meehan <garymeehan@outlook.com>
2022-10-20 19:18:48 +0100

    project id

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: 'cwqhzj',
+  projectId: 'upbk9f',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
