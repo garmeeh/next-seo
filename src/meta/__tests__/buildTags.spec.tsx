6be0302272faf9d265d945ec7f5477bbcf10f8d5
Gary Meehan <garymeehan@outlook.com>
2023-04-01 19:24:43 +0100

    Revert "Fix default robots overriding" (#1193)

--- src/meta/__tests__/buildTags.spec.tsx
+++ src/meta/__tests__/buildTags.spec.tsx
@@ -923,7 +923,7 @@ it('correctly sets nofollow default', () => {
     'meta[content="index,follow"]',
   );
   const noindexnofollow = container.querySelectorAll(
-    'meta[content="index,nofollow"]',
+    'meta[content="noindex,nofollow"]',
   );
 
   expect(Array.from(indexfollow).length).toBe(0);
@@ -945,8 +945,8 @@ it('correctly read noindex & nofollow false', () => {
     'meta[content="noindex,nofollow"]',
   );
 
-  expect(Array.from(indexfollow).length).toBe(1);
-  expect(Array.from(noindexnofollow).length).toBe(0);
+  expect(Array.from(indexfollow).length).toBe(0);
+  expect(Array.from(noindexnofollow).length).toBe(1);
 });
 
 it('correctly read all robots props', () => {
