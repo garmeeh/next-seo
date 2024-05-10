493264f19c5f6154940dfb4e5490dab4dfc90e89
francoismassart <francois.massart@gmail.com>
2024-05-10 11:18:20 +0200

    feat: `LiveBlogPosting`

--- cypress/e2e/article.spec.js
+++ cypress/e2e/article.spec.js
@@ -132,4 +132,64 @@ describe('Article JSON-LD', () => {
       });
     });
   });
+
+  it('matches schema when type LiveBlogPosting', () => {
+    Cypress.log({ message: 'azerty' });
+    cy.visit('http://localhost:3000/jsonld/liveBlogPosting');
+    cy.get('head script[type="application/ld+json"]').then(tags => {
+      const jsonLD = JSON.parse(tags[0].innerHTML);
+      assertSchema(schemas)('LiveBlogPosting', '1.0.0')(jsonLD);
+    });
+  });
+
+  it('renders with all props when type LiveBlogPosting', () => {
+    cy.visit('http://localhost:3000/jsonld/liveBlogPosting');
+    cy.get('head script[type="application/ld+json"]').then(tags => {
+      const jsonLD = JSON.parse(tags[0].innerHTML);
+      expect(jsonLD).to.deep.equal({
+        '@context': 'https://schema.org',
+        '@type': 'LiveBlogPosting',
+        datePublished: '2023-11-03T11:00:00+02:00',
+        description:
+          'Follow the announcements made during the 2023 Next.js Conf.',
+        mainEntityOfPage: {
+          '@type': 'WebPage',
+          '@id': 'https://example.com/liveblog',
+        },
+        headline: 'Next.js Conf live coverage',
+        image: [
+          'https://example.com/photos/1x1/photo.jpg',
+          'https://example.com/photos/4x3/photo.jpg',
+          'https://example.com/photos/16x9/photo.jpg',
+        ],
+        dateModified: '2023-11-03T19:00:00+02:00',
+        author: {
+          '@type': 'Person',
+          name: 'Jane Blogs',
+        },
+        coverageStartTime: '2023-11-03T11:00:00+02:00',
+        coverageEndTime: '2023-11-03T19:00:00+02:00',
+        liveBlogUpdate: [
+          {
+            '@type': 'BlogPosting',
+            headline: 'Next.js 14: No New APIs',
+            articleBody: 'Lorem ipsum dolor sit amet …',
+            datePublished: '2023-11-03T18:15:00+02:00',
+          },
+          {
+            '@type': 'BlogPosting',
+            headline: 'A faster, more personalized web.',
+            articleBody: 'Lorem ipsum dolor sit amet …',
+            datePublished: '2023-11-03T11:30:00+02:00',
+          },
+          {
+            '@type': 'BlogPosting',
+            headline: 'Conference will start soon',
+            articleBody: 'In a few hours …',
+            datePublished: '2023-11-03T11:00:00+02:00',
+          },
+        ],
+      });
+    });
+  });
 });
